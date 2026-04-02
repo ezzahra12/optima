0932eacf2625fb7d8afa7861f07ecedbe0ca3c39
ezzahra12 <blnfatima64@gmail.com>
2026-04-02 14:42:39 +0100

    Auth: Ajout du middleware CheckRole pour la gestion des accès

--- app/Http/Middleware/CheckRole.php
+++ app/Http/Middleware/CheckRole.php
@@ -13,8 +13,20 @@ class CheckRole
      *
      * @param  Closure(Request): (Response)  $next
      */
-    public function handle(Request $request, Closure $next): Response
-    {
-        return $next($request);
+   public function handle(Request $request, Closure $next, string ...$guards): Response
+{
+    $guards = empty($guards) ? [null] : $guards;
+
+    foreach ($guards as $guard) {
+        if (Auth::guard($guard)->check()) {
+            $user = Auth::user();
+            if ($user->role === 'admin') return redirect()->route('admin.index');
+            if ($user->role === 'chef') return redirect()->route('chef.dashboard');
+            if ($user->role === 'user') return redirect()->route('pending');
+            return redirect()->route('pending');
+        }
     }
+
+    return $next($request);
+}
 }
